c8b6b400b9de647b9642b378daad762bb2e82f86
lee.butler <lee.a.butler6.civ@mail.mil>
2019-07-05 14:00:16 -0400

    build pre-rm

--- misc/build.bat
+++ misc/build.bat
@@ -4,6 +4,7 @@
 
 SET MSBUILD="C:\Program Files\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\amd64\msbuild.exe"
 
+if exist ConsoleApp1\bin ( rmdir /s/q ConsoleApp1\bin )
 %MSBUILD% ConsoleApp1.sln /p:Configuration=Release
 if EXIST ConsoleApp1\bin\Release\ConsoleApp1.exe (
 ConsoleApp1\bin\Release\ConsoleApp1.exe
