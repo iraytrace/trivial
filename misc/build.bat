ff8f0c6992abd82777f17b469ce21d5a1f2a3431
lee.butler <lee.a.butler6.civ@mail.mil>
2019-07-05 13:49:07 -0400

    Release

--- misc/build.bat
+++ misc/build.bat
@@ -4,7 +4,7 @@
 
 SET MSBUILD="C:\Program Files\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\amd64\msbuild.exe"
 
-%MSBUILD% ConsoleApp1.sln
+%MSBUILD% ConsoleApp1.sln /p:Configuration=Release
 if EXIST ConsoleApp1\bin\Release\ConsoleApp1.exe (
 ConsoleApp1\bin\Release\ConsoleApp1.exe
 )
